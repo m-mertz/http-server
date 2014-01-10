f4fb9407b68b3ed9d06292070dc155dc3d2ad0e7
m-mertz <mertz.matthias@gmail.com>
2014-01-10 18:29:02 +0100

    Move send and recv functionality to sock class.

--- src/util/net/socket.h
+++ src/util/net/socket.h
@@ -10,13 +10,19 @@ class Socket {
   void Close();
   bool IsValid() const;
   int get_sock() const;
+  bool Send(const std::string msg) const;
+  std::string *Recv() const;
 
  private:
   // Disallow copy and assign.
   Socket(const Socket&);
   void operator=(const Socket&);
 
-  int sock_ = -1; // File descriptor of contained socket, -1 means invalid.
+  // Buffer size for recv operations. Must not be changed during a call of Recv,
+  // so just make it const altogether.
+  const int buff_size_ = 4096;
+  // File descriptor of contained socket, -1 means invalid.
+  int sock_ = -1;
 };
 
 #endif // UTIL_NET_SOCKET_H
